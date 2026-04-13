97893560718fb311bb4714ab2d44c67bbd057148
Christopher Reid <168359463+ChrisReid27@users.noreply.github.com>
2026-04-13 13:26:40 -0400

    Create show_tables.sql to display table contents
    Add SQL script to show tables for Student, Course, Enrollment, GradeCategory, Assignment, and Score.

---
+++ show_tables.sql
@@ -0,0 +1,10 @@
+-- ====================
+-- Task 3: Show Tables
+-- ====================
+
+SELECT * FROM Student;
+SELECT * FROM Course;
+SELECT * FROM Enrollment;
+SELECT * FROM GradeCategory;
+SELECT * FROM Assignment;
+SELECT * FROM Score;
